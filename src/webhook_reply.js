d2c2db57a65a30a8aae4184168eb819700218d01
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-20 17:33:33 +0000

    Commit from Transposit IDE

--- src/webhook_reply.js
+++ src/webhook_reply.js
@@ -13,7 +13,7 @@
     // Look for the parent message by thread timestamp
     let message = api.query("SELECT * FROM slack.get_channels_history WHERE channel=@channel AND ts=@ts", {channel: env.get('home'), ts: body.event.thread_ts})[0];
     /* If you would like to see the workspace id, it is stored below
-    let workspace = message.text; */
+    let workspace = message.blocks[1].block_id; */
 	api.run('this.post_away', {channel: message.blocks[0].block_id, text: body.event.text});
   }
   return { status_code: 200 };
