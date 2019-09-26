9c64cdc1591fb4af7016081ed9a9c71347900632
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-26 17:39:16 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,20 +1,22 @@
 ({ http_event }) => {
-  let body = http_event.parsed_body;
-  if (body.challenge) {
-    // https://api.slack.com/events/url_verification
-	return {
-      status_code: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: body.challenge
-    };
-  }
-  // Ignore bot users (mainly yourself)
-  if (!body.event.bot_id && !http_event.headers['X-Slack-Retry-Num']) {
-    let user = api.query("SELECT user.profile FROM slack_away.get_users_info WHERE user=@user", {user: body.event.user})[0].profile;
-    let team = api.run('slack_away.get_team_info')[0].team;
-    let title = `Message from *${user.real_name}* in workspace *${team.name}*:`;
-    // Set the home channel id you want the messages to send to in environment variables
-    api.run('this.post_home', {channel: env.get('home'), body: body.event.text, title: title, workspace: team.id, user: body.event.user});
-  }
+  setImmediate(() => {
+    let body = http_event.parsed_body;
+    if (body.challenge) {
+      // https://api.slack.com/events/url_verification
+      return {
+        status_code: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: body.challenge
+      };
+    }
+    // Ignore bot users (mainly yourself)
+    if (!body.event.bot_id && !http_event.headers['X-Slack-Retry-Num']) {
+      let user = api.query("SELECT user.profile FROM slack_away.get_users_info WHERE user=@user", {user: body.event.user})[0].profile;
+      let team = api.run('slack_away.get_team_info')[0].team;
+      let title = `Message from *${user.real_name}* in workspace *${team.name}*:`;
+      // Set the home channel id you want the messages to send to in environment variables
+      api.run('this.post_home', {channel: env.get('home'), body: body.event.text, title: title, workspace: team.id, user: body.event.user});
+    }
+  });
   return { status_code: 200 };
 }
--- src/webhook_reply.js
+++ src/webhook_reply.js
@@ -1,20 +1,22 @@
 ({ http_event }) => {
-  let body = http_event.parsed_body;
-  if (body.challenge) {
-    // https://api.slack.com/events/url_verification
-	return {
-      status_code: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: body.challenge
-    };
-  }
-  // Check for thread events only
-  if (body.event.thread_ts && !http_event.headers['X-Slack-Retry-Num']) {
-    // Look for the parent message by thread timestamp
-    let message = api.query("SELECT * FROM slack.get_channels_history WHERE channel=@channel AND ts=@ts", {channel: env.get('home'), ts: body.event.thread_ts})[0];
-    /* If you would like to see the workspace id, it is stored below
-    let workspace = message.blocks[1].block_id; */
-	api.run('this.post_away', {channel: message.blocks[0].block_id, text: body.event.text});
-  }
+  setImmediate(() => {
+    let body = http_event.parsed_body;
+    if (body.challenge) {
+      // https://api.slack.com/events/url_verification
+      return {
+        status_code: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: body.challenge
+      };
+    }
+    // Check for thread events only
+    if (body.event.thread_ts && !http_event.headers['X-Slack-Retry-Num']) {
+      // Look for the parent message by thread timestamp
+      let message = api.query("SELECT * FROM slack.get_channels_history WHERE channel=@channel AND ts=@ts", {channel: env.get('home'), ts: body.event.thread_ts})[0];
+      /* If you would like to see the workspace id, it is stored below
+      let workspace = message.blocks[1].block_id; */
+      api.run('this.post_away', {channel: message.blocks[0].block_id, text: body.event.text});
+    }
+  });
   return { status_code: 200 };
 }
